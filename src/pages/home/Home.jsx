4bac9f5ea39ac5b12de4b68edb9bea2b62cd20a4
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-12 17:31:32 +0300

    test

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,37 +1,45 @@
-import styles from "./home.module.scss";
-import { useEffect, useState } from "react";
+import styles from "./home.module.scss"
+import {useEffect, useState} from "react";
+
+
 
 const Home = () => {
+    const tg = window.Telegram.WebApp;
     const [data, setData] = useState('');
     const [userId, setUserId] = useState('');
     const [username, setUsername] = useState('');
+    let user = ''
 
     useEffect(() => {
-        const tg = window.Telegram?.WebApp;
-
-        if (tg) {
-            tg.ready(); // Ensure Telegram WebApp is initialized
-
-            const user = tg.initDataUnsafe?.user;
-            if (user) {
-                setUserId(user.id);
-                setUsername(user.username || "No username");
-                setData(user.first_name || "Anonymous");
-            }
-
-            console.log(user); // Debugging
+        tg.ready()
+        user = tg.initDataUnsafe.user;
+        if (user){
+            setUserId(user.id);
+            setUsername(user.username);
         }
-    }, []);
+        setData(tg.initDataUnsafe.user.username);
+        console.log(tg.initDataUnsafe.user);
+    }, [])
 
     return (
         <div className={styles.container}>
             <p className={styles.container_title}>Welcome to robots world</p>
             <h1>Авторизація через Telegram</h1>
-            <h1>Username: {data}</h1>
-            <h1>User ID: {userId}</h1>
-            <h1>Username: {username}</h1>
+            {/*<button onClick={handleClick}>Click</button>*/}
+            <h1>Username: ${data}</h1>
+            <h1>Username: ${username}</h1>
+            <h1>Username: ${userId}</h1>
+            {/*<script async src="https://telegram.org/js/telegram-widget.js?7"*/}
+            {/*        data-telegram-login="RobotsWorldBot"*/}
+            {/*        data-size="large"*/}
+            {/*        data-radius="10"*/}
+            {/*        data-auth-url="домен добавить бека!"*/}
+            {/*        data-color="#0088cc"*/}
+            {/*        data-prompt="true">*/}
+            {/*</script>*/}
+            {/*<div id="telegram-widget-container"></div>*/}
         </div>
-    );
-};
+    )
+}
 
-export default Home;
+export default Home
