f25e6ad8ee2a29e7b859408528cd1ed08a0a67b3
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-13 10:13:25 +0300

    test

--- bot/bot.js
+++ bot/bot.js
@@ -1,15 +1,23 @@
-import { Telegraf } from "telegraf";
-const TOKEN = "7633142965:AAE8zHTYJJT_r6L40-NO2ly0BDLWDghGLpg";
-const bot = new Telegraf(TOKEN);
+import {Telegraf} from 'telegraf';
 
-const web_link = "https://test-telegram-project-frontend.vercel.app/";
+const API_KEY = '7633142965:AAE8zHTYJJT_r6L40-NO2ly0BDLWDghGLpg';
+const bot = new Telegraf(API_KEY);
 
-bot.start((ctx) =>
-    ctx.reply("Welcome :)))))", {
-        reply_markup: {
-            keyboard: [[{ text: "web app", web_app: { url: web_link } }]],
-        },
-    })
-);
+bot.start((ctx) => {
+    const webAppButton = {
+        text: 'Open Web App',
+        web_app: {url: 'https://test-telegram-project-frontend.vercel.app//'}
+    };
 
-bot.launch();
+    const keyboard = {
+        inline_keyboard: [[webAppButton]]
+    };
+
+    ctx.reply("Welcome! Click the button below to open the web app:", {
+        reply_markup: keyboard
+    });
+});
+
+bot.launch()
+    .then(() => console.log('Bot is running...'))
+    .catch(err => console.error('Failed to launch the bot:', err));
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,42 +2,22 @@ import styles from "./home.module.scss"
 import {useEffect, useState} from "react";
 
 
-
 const Home = () => {
     const tg = window.Telegram.WebApp;
-    const [data, setData] = useState('');
-    const [userId, setUserId] = useState('');
     const [username, setUsername] = useState('');
-    let user = ''
-
     useEffect(() => {
-        tg.ready()
-        user = tg.initDataUnsafe.user;
-        if (user){
-            setUserId(user.id);
+        tg.ready();
+        const user = tg.initDataUnsafe?.user;
+        if (user && user.username) {
             setUsername(user.username);
+        } else {
+            setUsername("Guest");
         }
-        setData(tg.initDataUnsafe.user.username);
-        console.log(tg.initDataUnsafe.user);
-    }, [])
+    }, []);
 
     return (
         <div className={styles.container}>
-            <p className={styles.container_title}>Welcome to robots world</p>
-            <h1>Авторизація через Telegram</h1>
-            {/*<button onClick={handleClick}>Click</button>*/}
-            <h1>Username: ${data}</h1>
-            <h1>Username: ${username}</h1>
-            <h1>Username: ${userId}</h1>
-            {/*<script async src="https://telegram.org/js/telegram-widget.js?7"*/}
-            {/*        data-telegram-login="RobotsWorldBot"*/}
-            {/*        data-size="large"*/}
-            {/*        data-radius="10"*/}
-            {/*        data-auth-url="домен добавить бека!"*/}
-            {/*        data-color="#0088cc"*/}
-            {/*        data-prompt="true">*/}
-            {/*</script>*/}
-            {/*<div id="telegram-widget-container"></div>*/}
+            {username}
         </div>
     )
 }
