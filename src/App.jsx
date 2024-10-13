5f4cbf96fda1fbebb2c4a571285dfd2c122098dd
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-13 11:01:46 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Home from "./pages/home/Home.jsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import "./reset.scss"
+import Test from "./pages/Test.jsx";
 
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index path='/' element={<Home/>}/>
+                <Route index path='/' element={<Test/>}/>
             </Routes>
         </BrowserRouter>
     )
