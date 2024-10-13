8b134c62acbbf81b9f362246a118aa21b1bcd863
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-14 00:26:52 +0300

    test

--- src/components/btnLogin/BtnLogin.jsx
+++ src/components/btnLogin/BtnLogin.jsx
@@ -6,11 +6,12 @@ const BtnLogin = () => {
         window.onTelegramAuth = (user) => {
             console.log(user.id)
             console.log(user.first_name)
+            console.log(user)
             instance.post('/data', {
                 id: user.id,
                 first_name: user.first_name,
                 username: user.username,
-                authDate: user.authDate,
+                auth_date: user.auth_date,
                 hash: user.hash
             })
         };
