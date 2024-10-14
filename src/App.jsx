7896db0247988b38a165e365b2826f40ef492883
Viktor Lytvynenko <lytvynenkobooks@gmail.com>
2024-10-14 03:38:55 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/home/Home.jsx";
 import Users from "./pages/users/Users.jsx";
 import Profile from "./pages/profile/Profile.jsx";
 import {Provider} from "react-redux";
+import store from "./redux/store.js";
 
 
 const App = () => {
